1d0bc8fce60a161b545756be6f190f213b216276
Marco Antonio Soliz Lazarte <2023_marco.soliz@iticbcn.cat>
2023-11-03 16:29:17 +0100

    Reemplaza _ por caracter

--- code.js
+++ code.js
@@ -1,31 +1,48 @@
 function elPenjat() {
-    console.log("1. Iniciar un joc"
-        +"2. Estadistiques"
-        +"3. Sortir")
+    console.log("1. Iniciar un joc \n2. Estadistiques \n3. Sortir")
     let opcion = parseInt(prompt("Elige una opción"));
-    switch (opcion){
+    switch (opcion) {
         case 1:
-            let palabra = prompt("Escribe una palabra")
-            let longitudString = 0;
-            while(longitudString < palabra.length){
-                longitudString++;
-            }
-            let str = "_ ";
-            str.repeat(longitudString);
-            for(let i = 0; i <= 6; i++){
-                let letra = prompt("Escribe una Letra");
-                if(letra.length === 1){
-                    for(let x = 0; x < letra.length; x++){
-                        let comparaLetra = palabra[x];
-                        if(comparaLetra === letra){
-                            
+            let palabra = prompt("Escribe una palabra");
+            let arr = Array.from(palabra);
+            let str = "_ ".repeat(palabra.length);
+            let intentos = 6;
+
+            while (intentos > 0 && str.includes("_")) {
+                console.log(str);
+                let letra = prompt("Escribe una letra");
+
+                if (letra.length < 2 && letra.match(/[a-zA-Z]/)) {
+                    let encontrada = false;
+
+                    for (let x = 0; x < arr.length; x++) {
+                        if (arr[x] === letra) {
+                            str = str.substring(0, 2 * x) + letra + str.substring(2 * x + 1);
+                            encontrada = true;
                         }
                     }
-                }else{
-                    console.log("Incorrecto");
+
+                    if (!encontrada) {
+                        intentos--;
+                        console.log("Incorrecto. Te quedan " + intentos + " intentos.");
+                    }
+                } else {
+                    console.log("Entrada incorrecta. Introduce una sola letra.");
                 }
             }
+
+            if (!str.includes("_")) {
+                console.log("¡Has ganado! La palabra es: " + palabra);
+            } else {
+                console.log("¡Has perdido! La palabra era: " + palabra);
+            }
+
+        case 2:
+
+        case 3:
+            console.log("Juego terminado");
+            break;
     }
-    
-  //  alert(str.repeat(longitudString));
+
+    //  alert(str.repeat(longitudString));
 }
